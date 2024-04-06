84148c449de941d5d9f73d8d4717ca49813ae471
David Albert <davidbalbert@gmail.com>
2024-04-05 22:55:59 -0400

    WIP ls

--- main.go
+++ main.go
@@ -1,18 +1,177 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/davidbalbert/iso2next/fsutil"
 	"github.com/davidbalbert/iso2next/iso9660"
 )
 
+func usage() {
+	progname := os.Args[0]
+
+	fmt.Printf("Usage: %s <command> [options] [args]\n", progname)
+	fmt.Printf("Commands:\n")
+	fmt.Printf("  %s ls [-alfr] image_file [path]\n", progname)
+	fmt.Printf("  %s ls [-h]\n", progname)
+	fmt.Printf("  %s cp image_file source_path destination_path\n", progname)
+	fmt.Printf("  %s cp [-h]\n", progname)
+}
+
 func main() {
 	log.SetFlags(0)
 
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "ls":
+		ls()
+	case "cp":
+		cp()
+	case "help", "-h", "-help", "--help":
+		usage()
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+		usage()
+		os.Exit(1)
+	}
+}
+
+func ls() {
+	flags := flag.NewFlagSet("ls", flag.ExitOnError)
+
+	var aflag, lflag, fflag, Fflag, rflag bool
+
+	flags.BoolVar(&aflag, "a", false, "Show hidden files")
+	flags.BoolVar(&lflag, "l", false, "List files in long format")
+	flags.BoolVar(&fflag, "f", false, "Show full paths")
+	flags.BoolVar(&Fflag, "F", false, "Display identifying character after file name for special files")
+	flags.BoolVar(&rflag, "r", false, "Recursively list subdirectories (implies -f)")
+
+	err := flags.Parse(os.Args[2:])
+	if err == flag.ErrHelp {
+		fmt.Printf("Usage: %s ls [-alfr] image_file path\n", os.Args[0])
+		flags.PrintDefaults()
+		os.Exit(0)
+	} else if err != nil {
+		log.Fatal(err)
+	}
+
+	if rflag {
+		fflag = true
+	}
+
+	if flags.NArg() < 2 {
+		fmt.Printf("Usage: %s ls [-alfr] image_file path\n", os.Args[0])
+		flags.PrintDefaults()
+		os.Exit(1)
+	}
+
+	image := flags.Arg(0)
+	rootPath := flags.Arg(1)
+
+	rootPath = strings.TrimPrefix(rootPath, "/")
+	if rootPath == "" {
+		rootPath = "."
+	}
+
+	fsys, err := iso9660.Open(image)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fsys.Close()
+
+	err = fs.WalkDir(fsys, rootPath, func(path string, dirent fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if path == "." {
+			if rflag {
+				fmt.Println("/")
+			}
+			return nil
+		}
+
+		fname := dirent.Name()
+		if !rflag && path == rootPath && dirent.IsDir() {
+			fname = "."
+		}
+
+		if aflag || !strings.HasPrefix(fname, ".") {
+			suffix := ""
+			if Fflag {
+				switch dirent.Type() {
+				case fs.ModeDir:
+					suffix = "/"
+				case fs.ModeSymlink:
+					suffix = "@"
+				case fs.ModeSocket:
+					suffix = "="
+				case fs.ModeNamedPipe:
+					suffix = "|"
+				}
+			}
+
+			if fflag {
+				fmt.Println("/" + path + suffix)
+			} else {
+				fmt.Println(fname + suffix)
+			}
+		}
+
+		// never skip the children of the file/directory we're listing
+		if path == rootPath {
+			return nil
+		}
+
+		if dirent.IsDir() && (!rflag || (!aflag && strings.HasPrefix(fname, "."))) {
+			return fs.SkipDir
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cp() {
+
+}
+
+func formatMetadata(dirent fs.DirEntry) (string, error) {
+	var sb strings.Builder
+
+	info, err := dirent.Info()
+	if err != nil {
+		return "", err
+	}
+
+	sb.WriteString(info.Mode().String())
+	sb.WriteString("\t")
+
+	if info, ok := info.(fsutil.DeviceFileInfo); ok && dirent.Type() == fs.ModeDevice {
+		dev, err := info.Device()
+		if err != nil {
+			return "", err
+		}
+
+		sb.WriteString(fmt.Sprintf("%d, %d\t", dev.Major(), dev.Minor()))
+	} else {
+		sb.WriteString(fmt.Sprintf("%d\t", info.Size()))
+	}
+
+	return sb.String(), nil
+}
 	if len(os.Args) != 2 {
 		log.Fatalf("Usage: %s <iso9660 image>\n", os.Args[0])
 	}
